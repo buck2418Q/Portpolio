39233428acdd68def5a22958b5e7c21b57aecc13
buck2418Q <blackgem42@gmail.com>
2024-06-23 08:47:13 +0530

    section maintain

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,14 @@ import { FooterComponent } from "../footer/footer.component";
 import { ContactComponent } from "../contact/contact.component";
 import { ServiceComponent } from "../service/service.component";
 import { AboutComponent } from "../about/about.component";
+import { PortfolioComponent } from "../portfolio/portfolio.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [FooterComponent, ContactComponent, ServiceComponent, AboutComponent]
+    imports: [FooterComponent, ContactComponent, ServiceComponent, AboutComponent, PortfolioComponent]
 })
 export class HomeComponent {
 
